ff4f6d1b5822d1da3ff02e238fc503205cddae9f
Paul <paul.chynoweth@movember.com>
2020-08-06 08:17:22 +1000

    Updates

---
+++ precache-manifest.aa4fe0cc3b11afe238194994320ed2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5098d8f64341bb76698bf5774d534d3c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a807205327fc278aa03",
+    "url": "/monsters-rolodex/static/css/main.21cee958.chunk.css"
+  },
+  {
+    "revision": "4438d3e118dd3662f387",
+    "url": "/monsters-rolodex/static/js/2.d0f53c86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d0f53c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a807205327fc278aa03",
+    "url": "/monsters-rolodex/static/js/main.a25e83a5.chunk.js"
+  },
+  {
+    "revision": "d19dc7281abfb9c9947b",
+    "url": "/monsters-rolodex/static/js/runtime-main.cc0ccd30.js"
+  }
+]);
